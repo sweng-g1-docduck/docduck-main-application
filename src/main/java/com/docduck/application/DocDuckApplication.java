f989bca24c8ce3efa1d70e240e1f7581cab595bd
William-A-B <104766801+William-A-B@users.noreply.github.com>
2024-03-15 18:07:35 +0000

    Setup test main application

---
+++ src/main/java/com/docduck/application/DocDuckApplication.java
@@ -0,0 +1,47 @@
+package com.docduck.application;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class DocDuckApplication extends Application {
+
+    Stage mainStage;
+
+    public DocDuckApplication() {
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        DocDuckApplication myApp = new DocDuckApplication();
+
+        primaryStage.setScene(myApp.initialise());
+        primaryStage.setTitle("DocDuck");
+        primaryStage.show();
+    }
+
+    private Scene initialise() {
+
+        // Creating a Button
+        Button button = new Button();
+        // Setting text to the button
+        button.setText("Sample Button");
+        // Setting the location of the button
+        button.setTranslateX(150);
+        button.setTranslateY(60);
+        // Setting the stage
+        Group root = new Group(button);
+        Scene scene = new Scene(root, 595, 150, Color.BEIGE);
+
+        return scene;
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
