e988f87b4422769cbe905307ab00a0efeaa8033a
William-A-B <104766801+William-A-B@users.noreply.github.com>
2024-03-19 12:54:12 +0000

    Setup basic program

--- src/main/java/com/docduck/application/DocDuckApplication.java
+++ src/main/java/com/docduck/application/DocDuckApplication.java
@@ -3,14 +3,15 @@ package com.docduck.application;
 import com.docduck.application.parser.XMLReader;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class DocDuckApplication extends Application {
+
+    public static Stage myStage;
 
     public DocDuckApplication() {
     }
@@ -21,12 +22,20 @@ public class DocDuckApplication extends Application {
 
         DocDuckApplication myApp = new DocDuckApplication();
 
-        primaryStage.setScene(myApp.initialise());
-        primaryStage.setTitle("DocDuck");
-        primaryStage.show();
+        myStage = primaryStage;
 
-        XMLReader myReader = new XMLReader();
-        myReader.loadXML();
+        myStage.setScene(myApp.initialise());
+        myStage.setTitle("DocDuck Application");
+        myStage.show();
+
+        // ORDER OF PROGRAM
+        // Load up JavaFX
+        // Needs to check if there are any xml files to display a slide or slideshow
+        // If there are xml files, find ids of each and order them
+        // Display ID 1 slide
+        // If buttons, add in their actions, do they go to slide 2? etc.
+
+        XMLReader myReader = new XMLReader("src/main/resources/loginPage.xml", "src/main/resources/Standard.xsd", true);
         myReader.readXML();
         myReader.printXMLData();
 
@@ -34,7 +43,7 @@ public class DocDuckApplication extends Application {
 
     private Scene initialise() {
 
-        Image logo = new Image("./src/main/resources/docducklogo.png");
+//        Image logo = new Image("./src/main/resources/docducklogo.png");
 
         Button button = new Button();
         // Setting text to the button
@@ -44,7 +53,7 @@ public class DocDuckApplication extends Application {
         button.setTranslateY(60);
         button.setOnAction(e -> System.out.println("Hello World!"));
         // Setting the stage
-        Group root = new Group(button);
+        Pane root = new Pane(button);
 
         Scene scene = new Scene(root, 595, 150, Color.BEIGE);
 
@@ -55,4 +64,35 @@ public class DocDuckApplication extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    public static Stage getStage() {
+        return myStage;
+    }
+
+    // COMMAND LINE ARGUMENTS CODE:
+//    Parameters parameters = getParameters();
+//    List<String> args = parameters.getRaw();
+//
+//    String xmlPath = null;
+//    String schemaPath = null;
+//    boolean validate = false;
+//
+//    if (args.size() == 0) {
+//        System.out.println("Please specify command line arguments with -xml 'PATH_TO_XML' etc.");
+//    }
+//
+//    for (int i = 0; i < args.size(); i++) {
+//
+//        if (args.get(i) == "-xml") {
+//            xmlPath = args.get(i + 1);
+//        }
+//        else if (args.get(i) == "-xsd") {
+//            schemaPath = args.get(i + 1);
+//        }
+//        else if (args.get(i) == "-validate") {
+//            validate = true;
+//        }
+//    }
+//
+//    XMLReader myReader = new XMLReader(xmlPath, schemaPath, validate);
 }
--- src/main/java/com/docduck/application/parser/XMLReader.java
+++ src/main/java/com/docduck/application/parser/XMLReader.java
@@ -13,13 +13,21 @@ public class XMLReader {
 
     private Hashtable<String, Hashtable<String, Object>> xmlData = new Hashtable<>();
     private Parser myParser;
+    private String xmlPath;
+    private String schemaPath;
+    private boolean validate;
 
-    public XMLReader() {
+    public XMLReader(String xmlPath, String schemaPath, boolean validate) {
+        this.xmlPath = xmlPath;
+        this.schemaPath = schemaPath;
+        this.validate = validate;
 
+        loadParser();
     }
 
-    public void loadXML() {
-        this.myParser = new Parser("src/main/resources/loginPage.xml", "src/main/resources/standard.xsd", true);
+    private void loadParser() {
+        myParser = new Parser(xmlPath, schemaPath, validate);
+
     }
 
     public void readXML() {
