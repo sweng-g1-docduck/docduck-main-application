e1a0217be172bbc20d29753834e06b37387f878e
William-A-B <104766801+William-A-B@users.noreply.github.com>
2024-03-25 18:40:00 +0000

    Formatted code

--- src/main/java/com/docduck/application/gui/GUIBuilder.java
+++ src/main/java/com/docduck/application/gui/GUIBuilder.java
@@ -3,23 +3,24 @@ package com.docduck.application.gui;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
+import com.docduck.textlibrary.TextBox;
+
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
-import com.docduck.textlibrary.*;
 
 public class GUIBuilder {
-	
+
     private Hashtable<String, Hashtable<String, Object>> xmlData = new Hashtable<>();
     private Pane root;
-	
-	public GUIBuilder(Hashtable<String, Hashtable<String, Object>> xmlData) {
-		this.xmlData = xmlData;
-		
-	}
-	
-	public Node[] buildSlide(int slideNumber) {
-		boolean hasDataRemaining = true;
-		String textBox = "textBox";
+
+    public GUIBuilder(Hashtable<String, Hashtable<String, Object>> xmlData) {
+        this.xmlData = xmlData;
+
+    }
+
+    public Node[] buildSlide(int slideNumber) {
+        boolean hasDataRemaining = true;
+        String textBox = "textBox";
         String shape = "shape";
         String image = "image";
         String audio = "audio";
@@ -27,53 +28,60 @@ public class GUIBuilder {
         String delimiter = "-";
         int occurance = 1;
         ArrayList<Node> nodeList = new ArrayList<>();
-		while (hasDataRemaining == true) {
+
+        while (hasDataRemaining == true) {
             hasDataRemaining = false;
-            
+
             if (xmlData.containsKey(textBox + delimiter + slideNumber + delimiter + occurance) == true) {
-            	TextBox textbox = new TextBox();
-            	Hashtable<String,Object> textBoxData = xmlData.get(textBox + delimiter + slideNumber + delimiter + occurance);
-            	textbox.setWidth((Double)textBoxData.get("width"));
-            	textbox.setHeight((Double)textBoxData.get("height"));
-            	textbox.setFont((String)textBoxData.get("font"));
-            	textbox.setContent((String)textBoxData.get("text"));
-            	textbox.setFontSize((Integer)textBoxData.get("fontSize"));
-            	textbox.setFontColour((String)textBoxData.get("fontColour"));
-            	textbox.setCharacterSpacing((Double)textBoxData.get("characterSpacing"));
-            	textbox.setLineSpacing((Double)textBoxData.get("lineSpacing"));
-            	textbox.setPositionX((Integer)textBoxData.get("xCoordinate"));
-            	textbox.setPositionY((Integer)textBoxData.get("yCoordinate"));
+                TextBox textbox = new TextBox();
+                Hashtable<String, Object> textBoxData = xmlData
+                        .get(textBox + delimiter + slideNumber + delimiter + occurance);
+                textbox.setWidth((Double) textBoxData.get("width"));
+                textbox.setHeight((Double) textBoxData.get("height"));
+                textbox.setFont((String) textBoxData.get("font"));
+                textbox.setContent((String) textBoxData.get("text"));
+                textbox.setFontSize((Integer) textBoxData.get("fontSize"));
+                textbox.setFontColour((String) textBoxData.get("fontColour"));
+                textbox.setCharacterSpacing((Double) textBoxData.get("characterSpacing"));
+                textbox.setLineSpacing((Double) textBoxData.get("lineSpacing"));
+                textbox.setPositionX((Integer) textBoxData.get("xCoordinate"));
+                textbox.setPositionY((Integer) textBoxData.get("yCoordinate"));
                 nodeList.add(textbox.returnBox());
                 nodeList.add(textbox.returnText());
                 hasDataRemaining = true;
             }
 
             if (xmlData.containsKey(shape + delimiter + slideNumber + delimiter + occurance) == true) {
-                System.out.println("Shape-" + slideNumber + "-" + occurance + ": " + xmlData.get(shape + delimiter + slideNumber + delimiter + occurance));
+                System.out.println("Shape-" + slideNumber + "-" + occurance + ": "
+                        + xmlData.get(shape + delimiter + slideNumber + delimiter + occurance));
                 hasDataRemaining = true;
             }
 
             if (xmlData.containsKey(image + delimiter + slideNumber + delimiter + occurance) == true) {
-                System.out.println("Image-" + slideNumber + "-" + occurance + ": " + xmlData.get(image + delimiter + slideNumber + delimiter + occurance));
+                System.out.println("Image-" + slideNumber + "-" + occurance + ": "
+                        + xmlData.get(image + delimiter + slideNumber + delimiter + occurance));
                 hasDataRemaining = true;
             }
 
             if (xmlData.containsKey(audio + delimiter + slideNumber + delimiter + occurance) == true) {
-                System.out.println("Audio-" + slideNumber + "-" + occurance + ": " + xmlData.get(audio + delimiter + slideNumber + delimiter + occurance));
+                System.out.println("Audio-" + slideNumber + "-" + occurance + ": "
+                        + xmlData.get(audio + delimiter + slideNumber + delimiter + occurance));
                 hasDataRemaining = true;
             }
 
             if (xmlData.containsKey(video + delimiter + slideNumber + delimiter + occurance) == true) {
-                System.out.println("Video-" + slideNumber + "-" + occurance + ": " + xmlData.get(video + delimiter + slideNumber + delimiter + occurance));
+                System.out.println("Video-" + slideNumber + "-" + occurance + ": "
+                        + xmlData.get(video + delimiter + slideNumber + delimiter + occurance));
                 hasDataRemaining = true;
             }
             occurance++;
-		}
-		// Convert ArrayList to array
+        }
+        // Convert ArrayList to array
         Node[] nodes = new Node[nodeList.size()];
+
         for (int i = 0; i < nodeList.size(); i++) {
             nodes[i] = nodeList.get(i);
         }
         return nodes;
-	}
+    }
 }
