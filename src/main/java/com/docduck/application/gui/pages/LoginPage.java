56ac9b894e57ae16c01a0df55e37f8f661b4215f
htsm500 <htsm500@york.ac.uk>
2024-05-26 15:02:59 +0100

    set page size to window size, set colours

--- src/main/java/com/docduck/application/gui/pages/LoginPage.java
+++ src/main/java/com/docduck/application/gui/pages/LoginPage.java
@@ -7,16 +7,20 @@ import com.docduck.application.data.User;
 import com.docduck.buttonlibrary.ButtonWrapper;
 import com.docduck.textlibrary.TextBoxField;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class LoginPage extends Page {
 
-    private final Color signInBtnColour = Color.web("#007bffff");
+    private final Color signInBtnColour = Color.web("#fbb12eff");
     private final Color signInBtnHoverColour = Color.web("#0056b3ff");
     private final Color signInbtnClickColour = btnClickColour;
 
@@ -52,7 +56,7 @@ public class LoginPage extends Page {
         signUpBtn.setBackgroundColour(signUpBtnColour);
         signUpBtn.setHoverColour(signUpBtnHoverColour);
         signUpBtn.setClickcolour(signUpBtnClickColour);
-
+        buttons.setSpacing(10);
         buttons.getChildren().addAll(signUpBtn, signInBtn);
 
         int imageWidth = 400;
@@ -62,12 +66,14 @@ public class LoginPage extends Page {
         view.setFitWidth(imageWidth);
         view.setPreserveRatio(true);
 
-        TextBoxField usernameBox = drawTextField(300, 40, "Sign In");
-        TextBoxField passwordBox = drawTextField(300, 40, "Sign Up");
+        TextBoxField usernameBox = drawTextField(300, 40, "Username");
+        TextBoxField passwordBox = drawTextField(300, 40, "Password");
 
+        contents.setSpacing(15);
         contents.getChildren().addAll(view, usernameBox, passwordBox, buttons);
         setAlignment(contents, Pos.CENTER);
         setCenter(contents);
+        setBackground(new Background(new BackgroundFill(barColour, new CornerRadii(10), new Insets(5))));
 
     }
 
@@ -87,45 +93,22 @@ public class LoginPage extends Page {
         return textField;
     }
 
-//    public void drawTextBoxFields() {
-//
-//        TextBoxField usernameBox = drawTextField(300, 40, "Sign In");
-//        TextBoxField passwordBox = drawTextField(300, 40, "Sign Up");
-//
-//    }
-
     // layout: logo, two fields, 2 buttons, forgot password
     public void drawSigningButtons() {
         HBox buttons = new HBox();
 
-        // ButtonWrapper signInBtn = new ButtonWrapper();
-
         ButtonWrapper signInBtn = drawButtonWrapper(80, 40, "Sign In");
         ButtonWrapper signUpBtn = drawButtonWrapper(80, 40, "Sign Up");
-//        signInBtn.setButtonHeight(40);
-//        signInBtn.setButtonWidth(80);
-//        signInBtn.setCornerRadius(10);
-//        signInBtn.setFontColour(lightTextColour);
+
         signInBtn.setFontSize(14);
-//        signInBtn.setFontName(fontName);
-//        //signInBtn.setAlignment(Pos.CENTER_RIGHT);
         signInBtn.setBackgroundColour(signInBtnColour);
         signInBtn.setHoverColour(signInBtnHoverColour);
         signInBtn.setClickcolour(signInbtnClickColour);
-//        signInBtn.setText("Sign In");
 
-//        
-//        ButtonWrapper signUpBtn = new ButtonWrapper();
-//        signUpBtn.setButtonHeight(40);
-//        signUpBtn.setButtonWidth(80);
-//        signUpBtn.setCornerRadius(10);
-//        signUpBtn.setFontColour(lightTextColour);
         signUpBtn.setFontSize(14);
-//        signUpBtn.setFontName(fontName);
         signUpBtn.setBackgroundColour(signUpBtnColour);
         signUpBtn.setHoverColour(signUpBtnHoverColour);
         signUpBtn.setClickcolour(signUpBtnClickColour);
-//        signUpBtn.setText("Sign Up"); 
 
         buttons.getChildren().addAll(signUpBtn, signInBtn);
 
